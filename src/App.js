da6f197352c9f8c1d44df27dc8efcf495fd729ab
duongtai <theshy5893@gmail.com>
2025-08-06 16:16:31 +0700

    feat: set up route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                  {publicRoutes.map((route) => {
+                    const Page = route.component
+                      return <Route path={route.path} element={<Page />} />
+                  })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
